b22a9f8a84fe10b9f85a43f4ad48820b779aaec2
ethxndxvid <105835180+ethxndxvid@users.noreply.github.com>
2023-03-17 01:03:22 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-    fput("and that piece of art if useful\" - Dora Korpar, 2015-10-19\n", stdout);
+    fputs("and that piece of art if useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
