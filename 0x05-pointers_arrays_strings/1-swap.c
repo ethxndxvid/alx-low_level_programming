586fc0cc7f32d16896df3a5a402f8c94cd1e0c12
Ethan David <ethanbrian.david@gmail.com>
2023-03-28 08:52:51 +0200

    Typos

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers
+ * swap_int - swaps the values of the two integers
  * @a: pointer to the first value
  * @b: pointer to the second value
 */
 void swap_int(int *a, int *b)
 {
-    int c;
+	int c;
 
-    c = *a;
-    *a = *b;
-    *b = c;
+	c = *a;
+	*a = *b;
+	*b = c;
 }
