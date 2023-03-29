a44a1e00a8967e493d5335dee0b2447858fd7b30
Ethan David <ethanbrian.david@gmail.com>
2023-03-30 00:59:08 +0200

    Added all advanced tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ char *string_toupper(char *s);
 char *cap_string(char *str);
 char *leet(char *str);
 char *rot13(char *str);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
