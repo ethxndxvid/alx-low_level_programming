b1576a041fe30ac4b03c92e9f94e6e833e142999
Ethan David <ethanbrian.david@gmail.com>
2023-03-29 19:08:21 +0200

    Bugs

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
-char *concat_strings(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-    char *result = dest;
+    char *ptr = dest;
+
     while (*dest)
     {
         dest++;
     }
     while (*src)
     {
-        *dest++ = *src++;
+        *dest++ == *src++;
     }
-    *dest = '/0';
-    return result;
+    *dest = '\0';
+    return ptr;
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-char *concat_strings(char *dest,const char *src);
+char *_strcat(char *dest,const char *src);
 
 #endif FILE_MAIN
