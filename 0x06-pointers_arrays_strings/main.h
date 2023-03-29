4e6eb85b0f3cdd76cca42d4a6807e2e11e49d9bd
Ethan David <ethanbrian.david@gmail.com>
2023-03-29 22:49:28 +0200

    Added 4-rev_array.c and edited main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, const char *src, int n);
 char *_strncpy(char *dest, const char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 #endif
