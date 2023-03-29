8d9ec6bb1b2d5c61195deae89d928b5803368dbe
Ethan David <ethanbrian.david@gmail.com>
2023-03-29 23:45:41 +0200

    Reworked code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,34 +8,22 @@
 char *cap_string(char *str)
 {
 	int i, j;
-	char seperators[13] = {' ', '\t', '\n', ',', ';', '.',
+	char separators[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	for (i = 1; str[i] != '\0'; i++)
 	{
-		if (i == 0 || str[i - 1] == '\0')
+		for (j = 0; j < sizeof(separators) / sizeof(char); j++)
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if (str[i - 1] == separators[j])
 			{
-				str[i] -= 32;
-			}
-			else
-			{
-				for (j = 0; seperators[j] != '\0'; j++)
-				{
-					if (str[i - 1] == seperators[j])
-					{
-						if (str[i] >= 'a' && str[i] <= 'z')
-						{
-							str[i] -= 32;
-							break;
-						}
-					}
-				}
+				if (str[i] >= 'a' && str[i] <= 'z')
+					str[i] -= 32;
+				break;
 			}
 		}
-
 	}
 	return (str);
-
 }
