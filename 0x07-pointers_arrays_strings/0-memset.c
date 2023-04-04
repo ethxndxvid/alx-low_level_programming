7ffb3eb64a69d9b20097a25f22b60691d64aedf9
Ethan David <ethanbrian.david@gmail.com>
2023-04-04 20:09:08 +0200

    Betty doc

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * *_memset - fills memory with a constant byte
+ * @s : memory area to be filled
+ * @b: constant byte
+ * @n: number of bytes to be filled
+ * Return: pointer to the beginning of the memory area s
+ * 
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *ptr = s;
