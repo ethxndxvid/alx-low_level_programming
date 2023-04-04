0b17c6279e2806267600b68c8fc20144eca95d4b
Ethan David <ethanbrian.david@gmail.com>
2023-04-04 20:57:25 +0200

    Figured out how to #define NULL

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,19 +6,17 @@
  * @c: char to search: 
  * Return: pointer to the first occurence of the character c in the string s, or NULL if the character is not found.
 */
-char *_strchr(char *s, char c)
-{
-	while (*s!='\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	if (*s == c)
-	{
-		return (s);
-	}
-	
-}
+char *_strchr(char *s, char c) {
+    while (*s != '\0') {
+        if (*s == c) {
+            return s;
+        }
+        s++;
+    }
+
+    if (*s == c) {
+        return s;
+    }
+
+    return (NULL);
+} 
