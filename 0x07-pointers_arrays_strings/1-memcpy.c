0f8cf1762aad1508330d0577b793e746aae9d726
Ethan David <ethanbrian.david@gmail.com>
2023-04-04 20:21:19 +0200

    Updated main.h and added task.1 memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: sourceAdd message memory area
+ * @n: number of bytes to be copied
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ret = dest;
+
+	while (n-- )
+	{
+		*dest++ = *src++;
+	}
+	return (ret);
+}
