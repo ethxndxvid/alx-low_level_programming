d251b56d5524ce8c09bb1020f6caa5d645c582fc
Ethan David <ethanbrian.david@gmail.com>
2023-04-05 19:18:33 +0200

    Betty docs

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * _puts_recusion - prints a string, followed by a new line
+ * @s: string to print
+*/
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
@@ -7,6 +12,6 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	_putchar(s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
