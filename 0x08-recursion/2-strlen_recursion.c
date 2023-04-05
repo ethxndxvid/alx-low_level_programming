8b13cfd8ec3bb5702ce645ad922eeb86db1307d6
Ethan David <ethanbrian.david@gmail.com>
2023-04-05 19:46:54 +0200

    betty??

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,8 +3,7 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: pointer to string
- * 
- * Return : length of the string
+ * Return: length of the string
 */
 
 int _strlen_recursion(char *s)
