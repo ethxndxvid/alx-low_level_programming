7ff4ce990b963d0eb60fb6ec39901c2e29c17b82
Ethan David <ethanbrian.david@gmail.com>
2023-04-06 00:45:02 +0200

    Bugs

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to string
+ * Return: the length of the string
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
  * is_palindrome_helper - check if a string is a palindrome
  * @s: pointer to the string
  * @i: index of current character being checked
